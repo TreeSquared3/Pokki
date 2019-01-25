7402a9d1b4b7515a706f9c43111c136bb9df805f
Martin <martin.baumann99@protonmail.com>
2019-01-25 22:39:03 +0100

    Added pokemon classes- only Pokemon_Base finished so far

---
+++ Pokki/Classes/Pokemon_Active.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokki.Classes
+{
+    class Pokemon_Active : Pokemon_Base
+    {
+                      
+    }
+}
---
+++ Pokki/Classes/Pokemon_Base.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokki.Classes
+{
+    class Pokemon_Base
+    {
+        protected string name;
+        protected Pokemon_Base nextEvolution;
+        protected UInt16 levelNextEvolution;
+
+        public Pokemon_Base(string name, Pokemon_Base nextEvolution, UInt16 levelNextEvolution)
+        {
+            this.name = name;
+            this.nextEvolution = nextEvolution;
+            this.levelNextEvolution = levelNextEvolution;
+        }
+
+        #region "GET functions"
+
+        public string Name()
+        {
+            return this.name;
+        }
+
+        public Pokemon_Base NextEvolution()
+        {
+            return this.nextEvolution;
+        }
+
+        public UInt16 LevelNextEvolution()
+        {
+            return this.levelNextEvolution;
+        }
+
+        #endregion
+
+
+    }
+}
